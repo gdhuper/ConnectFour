e1dcb7d9f5598760b810be7f60e441ea41491d11
Gurpreet Singh <gdhuper@gmail.com>
2016-03-24 09:43:39 -0700

    Added Code for player class

--- Player.java
+++ Player.java
@@ -2,6 +2,19 @@ package CS151.HW3;
 
 public class Player {
 	
+	private String p1;
+	private String p2;
+	private String c1;
+	private String c2;
+	
+	
+	public Player(String name1, String color1, String name2, String color2)
+	{
+		this.p1 = name1;
+		this.c1 = color1;
+		this.p2 = name2; 
+		this.c2 = color2;
+	}
 
 	
 	
