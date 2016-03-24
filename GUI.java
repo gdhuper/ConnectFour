6cdce6a30882fe63326ddaf27b52f03b39b9e5e2
Gurpreet Singh <gdhuper@gmail.com>
2016-03-23 23:46:38 -0700

    Added the Menu bar

--- GUI.java
+++ GUI.java
@@ -8,6 +8,7 @@ import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -19,31 +20,23 @@ import javax.swing.border.EmptyBorder;
 
 public class GUI extends JFrame implements ActionListener{
 
-	private static final Graphics Graphics = null;
+	//private static final Graphics Graphics = null;
 	private JPanel contentPane;
-	private JMenuItem newGame, closeGame;
+	private JMenuItem newGame, closeGame, abt;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		
-		/**JFrame frame = new JFrame("Connect Four");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(new GridLayout(10,10));
 		
-		frame.add(new Button("click to play"));
-		frame.pack();
-		frame.setVisible(true);
-		*/
-		
-		
-		EventQueue.invokeLater(new Runnable() {
+	    EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					GUI frame = new GUI();
-					frame.setVisible(true);
-					//T4 gui = new T4(board, firstPlayer, depth);
+					frame.setLocationRelativeTo(null);
+					frame.setVisible(true);  // to center the frame
+					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -55,36 +48,38 @@ public class GUI extends JFrame implements ActionListener{
 	 * Create the frame.
 	 */
 	public GUI() {
-		JFrame frame = new JFrame();
+		//JFrame frame = new JFrame();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 500);
+		setBounds(200, 200, 800, 600);
 		setTitle("Connect Four!");
 		
-		JMenuBar menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
+		JMenuBar menuBar = new JMenuBar(); //For the menu on top 
+		
 		JMenu fileMenu = new JMenu("File");
 		
 	    newGame = fileMenu.add("New Game");
 	    newGame.addActionListener(this);
 	    closeGame = fileMenu.add("Quit");
-	   closeGame.addActionListener(this);
-	   JMenu aboutMenu = new JMenu("About");
-       
+	    closeGame.addActionListener(this);
+	    JMenu aboutMenu = new JMenu("About");
+        abt = aboutMenu.add("About Game");
+        abt.addActionListener(this);
 	   //JMenuItem closeItem = fileMenu.add("Close");
-	   fileMenu.addSeparator();
-	   menuBar.add(fileMenu); 
-	   menuBar.add(aboutMenu);
+	    fileMenu.addSeparator();
+	    menuBar.add(fileMenu); 
+	    menuBar.add(aboutMenu);
+	    setJMenuBar(menuBar);
 		
 	   contentPane = new JPanel();
-	   Font myFont = new Font("Serif", Font.BOLD, 50);
+	   // contentPane.setForeground(Color.cyan); // does nothing for right now 
+	   Font myFont = new Font("Sans-Serif", Font.BOLD, 100);
 	   JLabel label = new JLabel("Connect Four!");
 	   label.setFont(myFont);
-	  // contentPane.add(label);
-	   
 	  
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new GridLayout(10, 1));
-		//contentPane.add(drawBoard());
+		contentPane.setBorder(new EmptyBorder(20, 20, 5, 5));
+		contentPane.setLayout(new GridLayout(1, 1));
+		contentPane.setMaximumSize(new Dimension(Integer.MAX_VALUE,Integer.MIN_VALUE)); // to set the max size not sure how if works tho 	
+
 		setContentPane(contentPane);
 		getContentPane().setBackground(Color.YELLOW);
 		
@@ -92,11 +87,11 @@ public class GUI extends JFrame implements ActionListener{
 		
 	}
 	
-	public void drawBoard()
+	/**public void drawBoard()
 	{
 		Graphics.draw3DRect(10, 12, 23, 32, true);
 	
-	}
+	}*/
 	
 
 	@Override
@@ -106,10 +101,25 @@ public class GUI extends JFrame implements ActionListener{
 		{
 			//reset();
 		}
+		else if (e.getSource() == abt)
+		{
+			JOptionPane.showMessageDialog(null, "Welcome to Connect Four! Duh!");
+		}
 		else if(e.getSource() == closeGame)
 			
 		{
+			Toolkit.getDefaultToolkit().beep();
+			int decision = JOptionPane.showConfirmDialog(null, 
+			        "Are you sure want to quit the game?", "Quit",
+			        JOptionPane.YES_NO_OPTION);
+			if(decision == JOptionPane.YES_OPTION)
+			{
 			System.exit(0);
+			}
+			else
+			{
+				setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			}
 		}
 	}
 	
