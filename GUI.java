8e4ce382f02a0e038a3f421a9891f1b30f794c3b
Gurpreet Singh <gdhuper@gmail.com>
2016-03-27 16:41:14 -0700

    Working Game with correct Winner method

--- GUI.java
+++ GUI.java
@@ -1,9 +1,11 @@
- package CS151.HW3;
+package CS151.HW3;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.GridLayout;
@@ -15,109 +17,509 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
+public class GUI extends JFrame implements MouseListener, ActionListener{
 
-public class GUI extends JFrame implements ActionListener{
-
+	private JPanel panel_Center;
+	protected JPanel[][] buttonsBoard;
+	private static int boardSize = 8;
+	private static int winSequence = 5;
+	protected int [][] chess;
 	//private static final Graphics Graphics = null;
 	private MenuItem newGame, closeGame, abt;
-	BoardPanel p = new BoardPanel();
+	private int player = 1;
+	
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		//int size = Integer.parseInt(args[0]);
+		//if(size > 50)
+		//{
+			
+		//JOptionPane.showMessageDialog(null, "Sorry! Please select a size under 50");
+	//	}
+		//int winS = Integer.parseInt(args[1]);
+		//if(winS > size)
+		//{
+		//JOptionPane.showMessageDialog(null, "Sorry! Winning sequence should be less than board size");
+		//}
 		
-		BoardPanel.MAXCOL = Integer.parseInt(args[0]);
-		
-		
-		
-	    EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					
-
-					GUI frame = new GUI();
-					frame.setLocationRelativeTo(null);
-					frame.setVisible(true);  // to center the frame
-					
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		//new ConnectFour(Integer.parseInt(args[0]));
+	
+		new GUI(boardSize, winSequence );
 	}
 
-	/**
-	 * Create the frame.
-	 */
-	public GUI() {
-		//JFrame frame = new JFrame();
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(200, 200, 800, 600);
-		setTitle("Connect Four!");
-		
-		MenuBar menuBar = new MenuBar(); //For the menu on top 
-		
-		Menu fileMenu = new Menu("File");
-		
-	    newGame = new MenuItem("New Game");
-	    newGame.addActionListener(this);
-	    fileMenu.add(newGame);
-	    closeGame = new MenuItem("Quit");
-	    closeGame.addActionListener(this);
-	    fileMenu.add(closeGame);
-	    Menu aboutMenu = new Menu("About");
-        abt = new MenuItem("About Game");
-        abt.addActionListener(this);
-        aboutMenu.add(abt);
-	   //JMenuItem closeItem = fileMenu.add("Close");
-	    fileMenu.addSeparator();
-	    menuBar.add(fileMenu); 
-	    menuBar.add(aboutMenu);
-	    setMenuBar(menuBar);
-	    this.setVisible(true);
-	    
-	    
-	   //contentPane = new JPanel();
-	    Font myFont = new Font("Sans-Serif", Font.BOLD, 100);
-	    JLabel label = new JLabel("Connect Four!");
-	    label.setFont(myFont);
-	    label.setOpaque(true);
-	    
-	   
-	    
-	  
-		//contentPane.setBorder(new EmptyBorder(20, 20, 5, 5));
-		//contentPane.setLayout(new BorderLayout(1, 1));
-		//add(contentPane, BorderLayout.CENTER);
-		//.setMaximumSize(new Dimension(Integer.MAX_VALUE,Integer.MIN_VALUE)); // to set the max size not sure how if works tho 	
-		
-		//setContentPane(contentPane);
-		//getContentPane().setForeground(Color.BLUE);
-		//getContentPane().setBackground(Color.BLUE);
-		
-	   // BoardPanel p = new BoardPanel();
-	    //BoardPanel p = new BoardPanel(6, 7);
-	    p.setIgnoreRepaint(true);
-	    p.setPreferredSize(new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE));
-	     add(p, BorderLayout.CENTER);
-	    pack();
-	
-	    setSize(1024, 768);
-
-		
+	public GUI(int boardSize, int winSequence)
+	{
+		this.boardSize = boardSize;
+		this.winSequence = winSequence;
+	     showFrame();
+	     JOptionPane.showMessageDialog(null,"Welcome to Connect 4");
 	}
-	
+
+public void showFrame()
+{
+     setTitle("Connect 4");
+     setSize( boardSize*100,boardSize*100);
+     super.setLocationRelativeTo(null);
+     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
      
+     MenuBar menuBar = new MenuBar(); //For the menu on top 
+		
+	 Menu fileMenu = new Menu("File");
+		
+	 newGame = new MenuItem("New Game");
+	 newGame.addActionListener(this);
+	 fileMenu.add(newGame);
+	 closeGame = new MenuItem("Quit");
+	 closeGame.addActionListener(this);
+	 fileMenu.add(closeGame);
+	 Menu aboutMenu = new Menu("About");
+     abt = new MenuItem("About Game");
+     abt.addActionListener(this);
+     aboutMenu.add(abt);
+	//JMenuItem closeItem = fileMenu.add("Close");
+	 fileMenu.addSeparator();
+	 menuBar.add(fileMenu); 
+	 menuBar.add(aboutMenu);
+	 setMenuBar(menuBar);
+     
+   
+     
+    setLayout(new BorderLayout());
+    panel_Center=new JPanel();  //show the board
+    panel_Center.setLayout(new GridLayout(boardSize,boardSize,10,10));
+    panel_Center.setBackground(Color.YELLOW);
+    buttonsBoard =new JPanel[boardSize][boardSize];
+    chess=new int[boardSize][boardSize];
+        
+     for (int r=0; r<boardSize;r++)
+     {
+    	for (int c=0;c<boardSize;c++)
+            {
+                buttonsBoard[r][c]=new JPanel();
+               buttonsBoard[r][c].setBackground(Color.BLACK);
+               
+                panel_Center.add(buttonsBoard[r][c]);
+                chess[r][c] = 0;
+                
+                buttonsBoard[r][c].putClientProperty("row", r);
+                buttonsBoard[r][c].putClientProperty("col", c);
+                
+                buttonsBoard[r][c].addMouseListener(this);
+                chess[r][c]=0;	
+            }
+         }
+
+    add(panel_Center,BorderLayout.CENTER);
+    panel_Center.repaint();
+    setVisible(true);
+	}
+
 	
 
 	@Override
+	public void mouseClicked(MouseEvent e) 
+	{
+		JPanel panel = (JPanel) e.getSource();
+		int row = (Integer)panel.getClientProperty("row");
+		int col =  (Integer)panel.getClientProperty("col");
+		Component[] components = panel.getParent().getComponents();
+		for(int i=boardSize-1; i>=0; i-- ) {
+			if(chess[i][col] == 0) {
+				if(player == 0 || player == 1) {
+					chess[i][col] = 1;
+					for(Component component: components) {
+						JPanel jPanel = (JPanel)component;
+						if((Integer)jPanel.getClientProperty("row") == i && (Integer)jPanel.getClientProperty("col") == col ) {
+							jPanel.setBackground(Color.RED);
+							checkWinner();
+							player=2;
+							
+							break;
+						}
+					}
+					player=2;
+					break;
+				} else {
+					chess[i][col] = 2;
+					for(Component component: components) {
+						JPanel jPanel = (JPanel)component;
+						if((Integer)jPanel.getClientProperty("row") == i && (Integer)jPanel.getClientProperty("col") == col ) {
+							jPanel.setBackground(Color.BLUE);
+							checkWinner();
+							player=1;
+							
+							break;
+						}
+					}
+					
+					player=1;
+					break;
+				}
+			}
+		}
+		
+	}
+	
+	
+	public void checkWinner()
+	{
+
+		  
+		  //win at row
+		int counter = 0;
+		  for (int r=0;r<boardSize;r++) // 
+		    {
+		        for(int c=0;(c+(winSequence-1))<boardSize;c++)
+		             {
+		        	  for(int j = 0; j < winSequence; j++)  // for how many boxes to check around
+		        	  {
+		                if(chess[r][c]==chess[r][c+j]&&
+		                      
+		                      chess[r][c]==1)
+		                	
+		                        {
+		                			counter++;
+		                			continue;
+		                			
+		                        }
+		                else
+		                {
+		                	 counter = 0;
+		                	break;
+		                }
+		         
+		        	      }
+		        	         if(counter == winSequence)
+		        	         {
+		                         JOptionPane.showMessageDialog(null,player+" is a winner.");
+		                         counter = 0;
+		                         int d =  JOptionPane.showConfirmDialog(null, "Do you wish to replay game?", "Play Again!", JOptionPane.YES_NO_OPTION);
+			                        
+		                         if(d == JOptionPane.YES_OPTION)
+		                        	 {
+		                        		 reset();
+		                        	 }
+		                         else{
+		                        	 System.exit(0);
+		                         }
+		                        
+		        	         }
+		                        
+		                        
+		                         
+		                       
+
+		       
+		        for(int j = 0; j < winSequence; j++)   // for how many boxes to check around
+	        	  {
+		                 if(chess[r][c]==chess[r][c+j]&&
+		                       
+		                            chess[r][c]==2)
+		                      {
+		                	 counter++;
+		                	 continue;
+		                      }
+		                 else
+		                 {
+		                	 counter = 0;
+		                	 break;
+		                 }
+	        	  }
+		        		if(counter == winSequence)
+		        			{
+		                         JOptionPane.showMessageDialog(null,player+ " is a winner.");
+		                         counter = 0;
+		                         int q =  JOptionPane.showConfirmDialog(null, "Do you wish to replay game?", "Play Again!", JOptionPane.YES_NO_OPTION);
+			                        
+		                         if(q == JOptionPane.YES_OPTION)
+		                        	 {
+		                        		 reset();
+		                        	 }
+		                         else{
+		                        	 System.exit(0);
+		                         }
+		                        
+		                    }
+		             }
+		        }
+		 
+
+		  
+		   
+		   
+
+		 //win at column
+
+		 for (int r=0; (r+ (winSequence - 1))<boardSize;r++)
+		    {
+		         for(int c=0;c<boardSize;c++)
+		              {
+		        	 for(int j = 0; j < winSequence; j++) // for how many boxes to check around
+		        	  {
+		                 if(chess[r][c]==chess[r+j][c]&&
+		                       
+		                       chess[r][c]==1)
+		                         {
+		                	 counter++;
+		                	 continue;
+		                         }
+		                 else
+		                 {
+		                	 counter = 0;
+		                	 break;
+		                 }
+		        	  }
+		        	 if(counter == winSequence)
+		        	 {
+		                         JOptionPane.showMessageDialog(null, player+" is a winner.");
+		                         counter = 0;
+		                        int d =  JOptionPane.showConfirmDialog(null, "Do you wish to replay game?", "Play Again!", JOptionPane.YES_NO_OPTION);
+		                        
+		                         if(d == JOptionPane.YES_OPTION)
+		                        	 {
+		                        		 reset();
+		                        	 }
+		                         else{
+		                        	 System.exit(0);
+		                         }
+		        	 }
+		                         
+		                       
+		                    
+		        	 for(int j = 0; j < winSequence; j++) // for how many boxes to check around
+		        	  {
+		                  if(chess[r][c]==chess[r+j][c]&&
+		                             
+		                             chess[r][c]==2)
+		                       {
+		                	  counter++;
+		                	  continue;
+		                       }
+		                  else
+		                  {
+		                	  counter = 0;
+		                	  break;
+		                  }
+		        	  }
+		        	 if(counter == winSequence)
+		        	 {
+		                         JOptionPane.showMessageDialog(null,player+" is a winner.");
+		                         counter = 0;
+		                         int e =  JOptionPane.showConfirmDialog(null, "Do you wish to replay game?", "Play Again!", JOptionPane.YES_NO_OPTION);
+			                        
+		                         if(e == JOptionPane.YES_OPTION)
+		                        	 {
+		                        		 reset();
+		                        	 }
+		                         else{
+		                        	 System.exit(0);
+		                         }
+		                       
+		                    }
+		              }
+		         }
+		              
+		  
+		//win at upleft-to-downright direction
+		for (int r=0;(r+(winSequence - 1))<boardSize;r++)
+		    {
+		         for(int c=0;(c+(winSequence - 1))<boardSize;c++)
+		              {
+		        	 for(int j = 0; j < winSequence; j++)     // for how many boxes to check around
+		        	  {
+		                 if(chess[r][c]==chess[r+j][c+j]&&
+		                      
+		                       chess[r][c]==1)
+		                         {
+		                	 counter++;
+		                	 continue;
+		                         }
+		                 else
+		                 {
+		                	 counter = 0;
+		                	 break;
+		                 }
+		        	  }
+		        	 if(counter == winSequence)
+		        	 {
+		                         JOptionPane.showMessageDialog(null, player+" is a winner.");
+		                         counter = 0;
+		                         int d =  JOptionPane.showConfirmDialog(null, "Do you wish to replay game?", "Play Again!", JOptionPane.YES_NO_OPTION);
+			                        
+		                         if(d == JOptionPane.YES_OPTION)
+		                        	 {
+		                        		 reset();
+		                        	 }
+		                         else{
+		                        	 System.exit(0);
+		                         }
+		        	 }
+		                        
+		                    
+		        	 for(int j = 0; j < winSequence; j++)   // for how many boxes to check around
+		        	  {
+		                  if(chess[r][c]==chess[r+j][c+j]&&
+		                             
+		                             chess[r][c]==2)
+		                       {
+		                	  counter++;
+		                	  continue;
+		                       }
+		                  else
+		                  {
+		                	  counter = 0;
+		                	  break;
+		                  }
+		        	  }
+		                  
+		        	 if(counter == winSequence)
+		        	 {
+		                         JOptionPane.showMessageDialog(null,player+" is a winner.");
+		                         counter = 0;
+		                         int f =  JOptionPane.showConfirmDialog(null, "Do you wish to replay game?", "Play Again!", JOptionPane.YES_NO_OPTION);
+			                        
+		                         if(f == JOptionPane.YES_OPTION)
+		                        	 {
+		                        		 reset();
+		                        	 }
+		                         else{
+		                        	 System.exit(0);
+		                         }
+		                       
+		                    }
+		        	 
+		              }
+		         }
+		              
+		  
+		//win at downleft-to-upright direction
+		for (int r=boardSize - 1;(r-(winSequence - 1))>=0;r--)
+		    {
+		         for(int c=0;(c+(winSequence - 1))<boardSize;c++)
+		              {
+		        	 for(int j = 0; j < winSequence; j++)    // for how many boxes to check around
+		        	  {  
+		                 if(chess[r][c]==chess[r-j][c+j]&&
+		                       chess[r][c]==1)
+		                	 
+		                         {
+		                	 counter++;
+		                	 continue;
+		                         }
+		                 else
+		                 {
+		                	 counter = 0;
+		                	 break;
+		                 }
+		        	  }
+		        	 if(counter == winSequence)
+		        	 {
+		                         JOptionPane.showMessageDialog(null, player+" is a winner.");
+		                         counter = 0;
+		                         int d =  JOptionPane.showConfirmDialog(null, "Do you wish to replay game?", "Play Again!", JOptionPane.YES_NO_OPTION);
+			                        
+		                         if(d == JOptionPane.YES_OPTION)
+		                        	 {
+		                        		 reset();
+		                        	 }
+		                         else{
+		                        	 System.exit(0);
+		                         }
+		        	 }
+		                        
+		                    
+		        	 for(int j = 0; j < winSequence; j++)    // for how many boxes to check around
+		        	  {
+		                  if(chess[r][c]==chess[r-j][c+j]&&
+		                             
+		                             chess[r][c]==2)
+		                       {
+		                	  counter++;
+		                	  continue;
+		                       }
+		                  else
+		                  {
+		                	  counter = 0;
+		                	  break;
+		                  }
+		        	  }
+		        	    if(counter == winSequence)
+		        	    {
+		        	    	
+		        	    
+		                         JOptionPane.showMessageDialog(null,player+" is a winner.");
+		                         counter = 0;
+		                         int g =  JOptionPane.showConfirmDialog(null, "Do you wish to replay game?", "Play Again!", JOptionPane.YES_NO_OPTION);
+			                        
+		                         if(g == JOptionPane.YES_OPTION)
+		                        	 {
+		                        		 reset();
+		                        	 }
+		                         else{
+		                        	 System.exit(0);
+		                         }
+		        	    }
+		                       
+		                    }
+		         }
+		              }
+		  
+
+	
+
+		//end hasAwinner
+	
+
+	
+	
+	
+	
+	private void reset() {
+		for(int i = 0; i < boardSize; i++)
+		{
+			for(int j = 0; j < boardSize; j++)
+			{
+				this.chess[i][j] = 0;
+			}
+		}
+		new GUI(boardSize,winSequence );
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) 
+	{
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e)
+	{
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e)
+	{
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == newGame)
-		
 		{
 			//reset();
 		}
@@ -126,7 +528,6 @@ public class GUI extends JFrame implements ActionListener{
 			JOptionPane.showMessageDialog(null, "Welcome to Connect Four! Duh!");
 		}
 		else if(e.getSource() == closeGame)
-			
 		{
 			Toolkit.getDefaultToolkit().beep();
 			int decision = JOptionPane.showConfirmDialog(null, 
@@ -142,6 +543,4 @@ public class GUI extends JFrame implements ActionListener{
 			}
 		}
 	}
-	
-
 }
